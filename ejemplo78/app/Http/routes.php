7781330ba39cac7d423e10a54a9fefca906ff9be
firulais99 <luis_skate_dc@hotmail.com>
2015-06-30 14:48:19 -0600

    commit de joseluisitc

--- ejemplo78/app/Http/routes.php
+++ ejemplo78/app/Http/routes.php
@@ -12,5 +12,5 @@
 */
 
 Route::get('/', function () {
-    return 'Fuckkk youuuuuuuuu .l.!!!!<br/><br/>chiquillamividamiamorscosiwisiloviubeibi:*!!!!!!!!!';
+    return 'willkomen, ich hazze jose luis :)';
 });
